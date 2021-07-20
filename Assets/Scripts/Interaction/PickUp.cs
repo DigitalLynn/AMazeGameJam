4f2593d094eaf96a25b77fa0b5b8fbbe53cd7916
Henry Lynn <henrylynn98@gmail.com>
2021-07-20 22:18:38 +1000

    Blob now has interaction and digging

---
+++ Assets/Scripts/Interaction/PickUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private bool pickUpAllowed;
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+            DestroyPickUp();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpAllowed = false;
+        }
+    }
+
+    private void DestroyPickUp()
+    {
+        Destroy(gameObject);
+    }
+}
