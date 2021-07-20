6bad4ad41135f98d5bd740dd3eb45bc6b7c712a7
Anna <anna@pixplicity.com>
2021-07-20 15:47:31 +0200

    landing scene background and environment

---
+++ Assets/Scripts/Environment/SpriteOrder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOrder : MonoBehaviour {
+    private SpriteRenderer _sprite;
+
+    // Start is called before the first frame update
+    void Start () {
+        foreach (Transform child in transform) {
+            float zPosition = child.transform.position.z;
+            float order = zPosition * -100;
+
+            _sprite = child.GetComponent<SpriteRenderer> ();
+            if (_sprite) {
+                _sprite.sortingOrder = (int) order;
+            }
+
+        }
+
+    }
+}
