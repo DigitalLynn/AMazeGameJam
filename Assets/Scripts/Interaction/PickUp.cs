fcb99ad990e6b39bc5b2a907d593f18c01ee1a6e
Henry Lynn <henrylynn98@gmail.com>
2021-07-21 16:20:01 +1000

    Inventory system implimented

--- Assets/Scripts/Interaction/PickUp.cs
+++ Assets/Scripts/Interaction/PickUp.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     private bool pickUpAllowed;
+    public GameObject itemButton;
+
+    private Inventory inventory;
 
     // Use this for initialization
     private void Start()
     {
-
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
-            DestroyPickUp();
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    DestroyPickUp();
+                    break;
+                }
+            }
+            
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
