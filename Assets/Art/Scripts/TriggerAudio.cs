eeb588daccee47d7a2910853592653fda8b8e6b3
Henry Lynn <henrylynn98@gmail.com>
2021-07-30 15:50:09 +1000

    Final Upload (until we meet again)

---
+++ Assets/Art/Scripts/TriggerAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    public string playName;
+    public string pauseName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindObjectOfType<AudioManager>().Play(playName);
+        FindObjectOfType<AudioManager>().Pause(pauseName);
+    }
+}
